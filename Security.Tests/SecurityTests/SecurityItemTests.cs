67d564b353a04bcb93364552629831b2071ab667
José Manuel Cota <jmcota@ontica.org>
2025-10-31 09:06:50 -0400

    Security features, roles and subjects security items parse tests added

--- Security.Tests/SecurityTests/SecurityItemTests.cs
+++ Security.Tests/SecurityTests/SecurityItemTests.cs
@@ -11,6 +11,8 @@
 using Xunit;
 
 using Empiria.Security;
+using Empiria.Security.Data;
+using Empiria.Security.Subjects;
 
 namespace Empiria.Tests.Security {
 
@@ -18,6 +20,51 @@ namespace Empiria.Tests.Security {
   public class SecurityItemTests {
 
     #region Facts
+
+    [Fact]
+    public void Should_Parse_All_Features() {
+      var features = BaseObject.GetFullList<Feature>();
+
+      Assert.NotNull(features);
+
+      foreach (var feature in features) {
+        Assert.NotNull(feature.Requires);
+        Assert.NotNull(feature.ObjectsGrants);
+      }
+    }
+
+
+    [Fact]
+    public void Should_Parse_All_Roles() {
+      var roles = BaseObject.GetFullList<Role>();
+
+      Assert.NotNull(roles);
+
+      foreach (var role in roles) {
+        Assert.NotNull(role.Grants);
+        Assert.NotNull(role.ObjectsGrants);
+        Assert.NotNull(role.Revokes);
+      }
+    }
+
+
+    [Fact]
+    public void Should_Parse_All_Subjects_Security_Items() {
+      FixedList<SubjectData> subjects = SubjectsDataService.SearchSubjects(string.Empty);
+
+      Assert.NotNull(subjects);
+      Assert.NotEmpty(subjects);
+
+      foreach (var subject in subjects.Select(x => x.Contact)) {
+        var subjectContexts = SecurityContext.GetList(subject);
+
+        foreach (var context in subjectContexts) {
+          Assert.NotNull(Role.GetSubjectRoles(subject, context));
+          Assert.NotNull(Feature.GetSubjectFeatures(subject, context));
+        }
+      }
+    }
+
 
     [Fact]
     public void Should_Read_All_Security_Items() {
